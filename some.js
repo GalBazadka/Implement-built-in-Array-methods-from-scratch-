16a2000173c7224a899ae3d133479ed76f771f78
Gal Bazadka <galbazadka@gmail.com>
2022-10-13 22:33:01 +0300

    Create some.js

---
+++ some.js
@@ -0,0 +1,20 @@
+const myCustomSomeFunction = function (callback) {
+  console.log("My Custom some Function!");
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      return true;
+    }
+  }
+  return false;
+};
+
+Array.prototype.someFromScratch = myCustomSomeFunction;
+
+const numbers = [5, 3, 5, 8, 5];
+const result = numbers.someFromScratch(checkAdult);
+console.log(result);
+
+function checkAdult(age) {
+  return age >= 18;
+}
