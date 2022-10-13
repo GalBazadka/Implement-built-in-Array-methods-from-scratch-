909954b22f1f8915659f607ed5dbf20b50525c54
Gal Bazadka <galbazadka@gmail.com>
2022-10-13 22:32:46 +0300

    Create find.js

---
+++ find.js
@@ -0,0 +1,20 @@
+const myCustomFindFunction = function (callback) {
+  console.log("My Custom find Function!");
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i])) {
+      return this[i];
+    }
+  }
+  return undefined;
+};
+
+Array.prototype.findFromScratch = myCustomFindFunction;
+
+const numbers = [16, 4, 9, 20, 50, 10];
+const value = numbers.findFromScratch(checkAdult);
+console.log(value);
+
+function checkAdult(age) {
+  return age >= 18;
+}
